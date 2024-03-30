928f67e82b4afc991ccd0cf73e6fdafd39744a84
Alessandro <aleworkingofficial@gmail.com>
2024-03-30 13:00:44 -0300

    Used the spread to create an array

--- set.js
+++ set.js
@@ -2,7 +2,7 @@ let myArray = [30, 30, 40, 5, 225, 2039, 2045, 5]
 
 function noRepetitions(array){
     let mySet = new Set(array)
-    return mySet
+    return [...mySet]
 }
 
 console.log(noRepetitions(myArray))
